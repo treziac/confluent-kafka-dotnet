33d6821cde5e026b0e5c530e065726ab4eea56ca
treziac <aymeric.bouvet@outlook.com>
2017-06-01 08:03:12 +0200

    Add RunContinuationsAsynchronously to TaskDeliveryHandler
    Add net46 as target framework to have this on as many target as possible
    RunContinuationsAsynchronously will reduce the overhead (almost null) in regard of Task.Run

--- src/Confluent.Kafka/Producer.cs
+++ src/Confluent.Kafka/Producer.cs
@@ -182,11 +182,19 @@ namespace Confluent.Kafka
 
         private sealed class TaskDeliveryHandler : TaskCompletionSource<Message>, IDeliveryHandler
         {
+#if !NET45
+            public TaskDeliveryHandler() : base(TaskContinuationOptions.RunContinuationsAsynchronously)
+            { }
+#endif
             public bool MarshalData { get { return true; } }
 
             public void HandleDeliveryReport(Message message)
             {
+#if NET45
                 System.Threading.Tasks.Task.Run(() => SetResult(message));
+#else
+                SetResult(message);
+#endif
             }
         }
 
@@ -806,6 +814,9 @@ namespace Confluent.Kafka
         private class TypedTaskDeliveryHandlerShim : TaskCompletionSource<Message<TKey, TValue>>, IDeliveryHandler
         {
             public TypedTaskDeliveryHandlerShim(TKey key, TValue val)
+#if !NET45
+                : base(TaskContinuationOptions.RunContinuationsAsynchronously)
+#endif
             {
                 Key = key;
                 Value = val;
@@ -829,7 +840,11 @@ namespace Confluent.Kafka
                     message.Error
                 );
 
+#if NET45
                 System.Threading.Tasks.Task.Run(() => SetResult(mi));
+#else
+                SetResult(mi);
+#endif
             }
         }
 
